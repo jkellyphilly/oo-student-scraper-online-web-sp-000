198beb532ac55cbfc7e53254ec1ba9e7bfc8bf0e
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-07 02:08:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each do |attribute|
-
-    end
+    student_hash.each {|key, value| self.send("#{key}=", value)}
   end
 
   def self.create_from_collection(students_array)
